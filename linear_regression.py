fdf0f9b0a00f3552acfe32ad3be42b2251dadfb1
danulg <danulg@gmail.com>
2025-07-15 21:01:59 -0400

    Code for loading data

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,17 @@
-import sklearm
+#import require libraries
+
+import sklearn
+import os
 import pandas as pd
 import numpy as np
 import matplotlib as plot
 
+#check working directory and set paths for data location as needed
+print(os.getcwd()) # Data in current working directory as data set is small.
+redw_path = "/home/danulg/Git/Data604/lr-redw.csv"
+whitew_path = "/home/danulg/Git/Data604/lr-whitew.csv"
+
+# Load data 
+red_wine = pd.read_csv(redw_path, sep = ";")
+white_wine = pd.read_csv(whitew_path, sep = ";")
 

74922dcec3680be6f827489b23fd4a809722cd89
danulg <danulg@gmail.com>
2025-07-17 10:50:23 -0400

    comments on structure / goals added

--- linear_regression.py
+++ linear_regression.py
@@ -15,3 +15,13 @@ whitew_path = "/home/danulg/Git/Data604/lr-whitew.csv"
 red_wine = pd.read_csv(redw_path, sep = ";")
 white_wine = pd.read_csv(whitew_path, sep = ";")
 
+# We will limit the analysis to just red wine here. We start by splitting our data into train and test sets.
+# We could include a validation set as well, but this will add another level of complexity. We avoid this for 
+# the time being. 
+# 
+# Our goal is to build a model that works well on unseen data. For this reason, we will not touch the test set
+# at this point 
+
+# Exploratory data analysis
+# Test / validation /  training splits are done under the assumption that random subsets of the data have 
+# similar structure. If not, the 
